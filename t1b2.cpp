31971e644f73c7df1934dbd5ef79e34003c8637f
Nghia <nghiak99lhp@gmail.com>
2022-02-21 16:32:50 +0700

    4 commit

---
+++ t1b2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Monday" << endl
+        << "Tuesday" << endl
+        << "Wednesday" << endl
+        << "Thursday" << endl
+        << "Friday" << endl
+        << "Saturday" << endl
+        << "Sunday" << endl;
+    return 0;
+}
