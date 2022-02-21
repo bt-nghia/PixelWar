5112004d017a28a21366fd7d4707265204d744a5
Nghia <nghiak99lhp@gmail.com>
2022-02-21 16:39:21 +0700

    7 commit

---
+++ t1b6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include<string>
+
+using namespace std;
+
+int main() {
+    string a,b,c;
+    cin >> a >> b >> c;
+    cout << "Hi " << c << ", " << b << " and " << a << ".";
+    return 0;
+}
