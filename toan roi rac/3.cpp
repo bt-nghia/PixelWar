11919dbe8b2009486e7efccbb1abe07dcb88c086
bt-nghiauet <21020364@vnu.ednu.vn>
2022-03-30 12:22:53 +0700

    fix p3

--- toan roi rac/3.cpp
+++ toan roi rac/3.cpp
@@ -2,9 +2,15 @@
 
 using namespace std;
 
-
+int m(int n) {
+    if(n < 10) {return n;}
+    else {
+        int x = n % 10;
+        return min(m(n/10), x);
+    }
+}
 
 int main() {
-    string num = "121629861982619863";
-
+    cout << m(345);
+    return 0;
 }
